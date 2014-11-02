b5fcd3d925f74867d1f162e4206111546f0b02f6
tarunjohnmathew <tarun@mathewj.com>
2014-11-01 20:38:55 -0700

    my new farenhiet to celcius

--- python/first.py
+++ python/first.py
@@ -1,4 +1,7 @@
-print("what is the temprature conversion from celciues to farenheight?")
-farenheit = input()
-fInt = int(farenheit)
-print(farenheit + "in celcius is" + celcius)
+print("what is the temperature conversion from celciues to fahrenheit?")
+fahrenheit = input()
+fInt = int(fahrenheit)
+Celcius= (fInt-32)*5/9
+print(fahrenheit + " in celcius is " + str(Celcius))
+
+
