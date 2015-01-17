23001c8551dce0141b74ddec0316d346bfde3297
tarunjohnmathew <tarun@mathewj.com>
2015-01-17 11:10:49 -0800

    how to make loops, ariables, and math operations

---
+++ python/how to make Variables.py
@@ -0,0 +1,6 @@
+x=0
+while x< 10:
+    y=37*x - 5
+    print("x y = ", x,y)
+x=x+1
+          
---
+++ python/how to make loops.py
@@ -0,0 +1,21 @@
+print("Building a table for y = 37x - 5")
+
+x=0
+y=37*x-5
+print("x, y = ", x, y)
+
+x=1
+y=37*x-5
+print("x, y = ", x, y)
+
+x=2
+y=37*x-5
+print("x, y = ", x, y)
+
+x=3
+y=37*x-5
+print("x, y = ", x, y)
+
+x=5
+y=37*x-5
+print("x, y = ", x, y)
